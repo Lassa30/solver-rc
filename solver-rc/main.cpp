756790e08df63a9e44836b1df0baa6218cbc758c
Lassa30 <loskutovvlad@gmail.com>
2025-04-28 10:26:21 +0300

    make initial setup and helloworld imgui app

---
+++ solver-rc/main.cpp
@@ -0,0 +1,46 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+
+int main() {
+    // Setup GLFW
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ImGui Minimal", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    // Setup ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");  // GLSL version
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // Start new ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Your UI
+        ImGui::ShowDemoWindow();  // Show built-in demo window
+
+        // Render
+        ImGui::Render();
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h);
+        glViewport(0, 0, w, h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
