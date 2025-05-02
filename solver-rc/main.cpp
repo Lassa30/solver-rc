7c339b0e5a087671fe60a46f22e1c24437219fc5
Lassa30 <loskutovvlad@gmail.com>
2025-05-03 01:46:54 +0300

    refactor the code: make shaders separate, add comments

--- solver-rc/main.cpp
+++ solver-rc/main.cpp
@@ -1,3 +1,5 @@
+#include "utils.hpp"
+
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <imgui.h>
@@ -12,6 +14,20 @@
 #include <chrono>
 #include <iostream>
 #include <random>
+
+/*
+GL RELATED UTILS
+*/
+static void glfw_error_callback(int error, const char *description) {
+  std::cout << (stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+static void printShaderRelatedError(int &infoLength, GLuint &shaderProgram) {
+  std::vector<char> ProgramErrorMessage(infoLength + 1);
+  glGetProgramInfoLog(shaderProgram, infoLength, nullptr,
+                      &ProgramErrorMessage[0]);
+  printf("%s\n", &ProgramErrorMessage[0]);
+}
 
 /*
 ***************************************************
@@ -33,11 +49,6 @@ enum class ColorID { Color_1, Color_2, Color_3, Color_4, Color_5, Color_6, Color
 // };
 
 struct Cubie {
-  /*colors of a cubie:
-  center - 1 color
-  edge - 2 colors
-  corner - 3 colors
-  */
   std::array<ColorID, 3> colors;
   glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
 
@@ -51,117 +62,71 @@ class RubiksCube {
 class CubeRenderer;
 class CubieRenderer;
 
-std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_real_distribution<> distrib(0, 1);
+static float x = 0.5f;
 
-static void glfw_error_callback(int error, const char *description) {
-  std::cout << (stderr, "GLFW Error %d: %s\n", error, description);
-}
-
-float r1 = 0;
-float g1 = 1;
-float b1 = 0;
-
-float r2 = 1.0;
-float g2 = 0;
-float b2 = 1.0;
-float x = 0.1f;
-
-float Vertices[3][7] = {{-x, x, 0, r1, g1, b1, 1},
-                        {x, x, 0, 0, 0, 0, 1},
-                        {x, -x, 0, r2, g2, b2, 1}};
+static float Vertices[3][7] = {
+    {-x, x, 0, 1, 0, 0, 1}, {x, x, 0, 1, 0, 0, 1}, {x, -x, 0, 1, 0, 0, 1}};
 
 void do_render(GLuint VAO, GLuint VBO) {
   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
+
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertices), Vertices);
   glBindVertexArray(VAO);
-  glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+  glDrawArrays(GL_TRIANGLE_FAN, 0, 3);
 }
 
 GLuint compileShaders() {
-  /*glsl language to write shaders
-  vertex shader: sets the position and the color for the each vertex
-  */
+  GLint compileStatus;
+  int infoLength;
 
-  /*scale-rotate-translate vertexShader
-  this code should better be outside of the shader
-  shader code better be just: MVP * vect
-  */
-  const char *vertexShaderSource = R"glsl(
-    #version 460 core
-    layout (location = 0) in vec3 aPos;
-    layout (location = 1) in vec4 aColor;
-    out vec4 vertexColor;
-    
-    uniform float time;
-    float k = abs(sin(time)) + 0.5;
-    mat4 scale = mat4(
-      vec4(k, 0, 0, 0),
-      vec4(0, k, 0, 0),
-      vec4(0, 0, 1, 0),
-      vec4(0, 0, 0, 1)
-    );
+  // reading vertex shader
+  std::string vertexShaderSource =
+      readShader("../solver-rc/shaders/vertexShader.vert");
+  const char *vertexShaderSource_cstr = vertexShaderSource.c_str();
 
-    uniform float alpha;  
-    mat4 rotation = mat4(
-      vec4(cos(alpha), sin(alpha), 0, 0),
-      vec4(-sin(alpha), cos(alpha), 0, 0),
-      vec4(0, 0, 1, 0),
-      vec4(0, 0, 0, 1)
-    );
+  // reading fragment shader
+  std::string fragmentShaderSource =
+      readShader("../solver-rc/shaders/fragmentShader.frag");
+  const char *fragmentShaderSource_cstr = fragmentShaderSource.c_str();
 
-    uniform float dx;
-    uniform float dy;
-    mat4 translation = mat4(
-          vec4(1, 0, 0, 0),
-          vec4(0, 1, 0, 0),
-          vec4(0, 0, 1, 0),
-          vec4(dx, dy, 0, 1)
-    );
-
-    void main() {
-        gl_Position = translation * rotation * scale * vec4(aPos, 1.0);
-        vertexColor = aColor;
-    })glsl";
-  /*fragment shader: takes input from the vertex shader
-  and then do the stuff
-
-  nb.:
-  FragColor = vec4(...) is actually a cycle
-  for fragment in all_fragments:
-    FragColor[F] = vec4(...)
-  */
-  const char *fragmentShaderSource = R"glsl(
-    #version 460 core
-    in vec4 vertexColor;
-    out vec4 FragColor;
-
-    uniform float time;
-    void main() {
-        FragColor = vec4(
-          vertexColor.x + abs(sin(time)),
-          vertexColor.y,
-          vertexColor.z,
-          1.0
-        );
-    })glsl";
-
+  // compiling vertex shader
   GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glShaderSource(vertexShader, 1, &vertexShaderSource_cstr, nullptr);
   glCompileShader(vertexShader);
+  // checking vertex shader
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compileStatus);
+  glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &infoLength);
+  if (infoLength > 0) {
+    printShaderRelatedError(infoLength, vertexShader);
+  }
 
+  // compiling fragment shader
   GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource_cstr, nullptr);
   glCompileShader(fragmentShader);
+  // checking fragment shader
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &compileStatus);
+  glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &infoLength);
+  if (infoLength > 0) {
+    printShaderRelatedError(infoLength, fragmentShader);
+  }
 
+  // linking shader program
   GLuint shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
   glLinkProgram(shaderProgram);
 
+  // check the program
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &compileStatus);
+  glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLength);
+  if (infoLength > 0) {
+    printShaderRelatedError(infoLength, shaderProgram);
+  }
+
+  // delete shaders: they are compiled already
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
@@ -169,23 +134,28 @@ GLuint compileShaders() {
 }
 
 int main() {
-  glfwSetErrorCallback(glfw_error_callback);
-  if (!glfwInit())
-    return 1;
+  const int xSideSize = 800;
+  const int ySideSize = 600;
 
-  GLFWwindow *window =
-      glfwCreateWindow(1200, 1200, "Rubik's Cube Solver", nullptr, nullptr);
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit()) {
+    return 1;
+  }
+  // clang-format off
+  GLFWwindow *window = glfwCreateWindow(xSideSize, ySideSize, "Rubik's Cube Solver", nullptr, nullptr);
+  // clang-format on
   if (!window) {
     return 1;
   }
 
   glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);
+  glfwSwapInterval(1); // vsync on
 
   if (!gladLoadGL(glfwGetProcAddress)) {
     std::cerr << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
+
   // compiling shaders
   auto shaderProgram = compileShaders();
   glUseProgram(shaderProgram);
@@ -200,54 +170,26 @@ int main() {
 
   glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_DYNAMIC_DRAW);
 
-  // telling how to interpret the data inside shaders
+  // size related
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
-  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float),
-                        (void *)(3 * sizeof(float)));
+  // size related
+  // clang-format off
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void *)(3 * sizeof(float)));
+  // clang-format on
   glEnableVertexAttribArray(1);
 
   // main loop
-  bool isFirst = true;
-  float time = 0.0f;
-  int alpha = 0;
-  auto prev_time = std::chrono::high_resolution_clock::now();
-  auto curr_time = prev_time;
-  auto timedelta = std::chrono::seconds(1);
-
-  auto alphaID = glGetUniformLocation(shaderProgram, "alpha");
-  auto dxID = glGetUniformLocation(shaderProgram, "dx");
-  auto dyID = glGetUniformLocation(shaderProgram, "dy");
-  auto timeID = glGetUniformLocation(shaderProgram, "time");
-
   while (!glfwWindowShouldClose(window)) {
     // resize handling
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
-    // uniforms for shaders
-    auto dx = (distrib(gen) - 0.5) * 2;
-    glUniform1f(dxID, dx);
+    do_render(VAO, VBO);
 
-    auto dy = (distrib(gen) - 0.5) * 2;
-    glUniform1f(dyID, dy);
-
-    glUniform1f(timeID, time);
-    glUniform1f(alphaID, M_PI * alpha / 180);
-
-    // rendering
-    auto curr_time = std::chrono::high_resolution_clock::now();
-    if (isFirst || (curr_time - prev_time > timedelta)) {
-      do_render(VAO, VBO);
-      glfwSwapBuffers(window);
-      prev_time = curr_time;
-      isFirst = false;
-    }
-    // update
-    time += 0.01f;
-    alpha = (alpha >= 360) ? 0 : alpha + 1;
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
@@ -255,3 +197,34 @@ int main() {
   glfwTerminate();
   return 0;
 }
+
+/*
+[DEFINITIONS]
+  glsl           : language to write shaders
+  vertex shader  : sets the position and the color for the each vertex
+  fragment shader: takes input from the vertex shader and then do the stuff
+
+[NOTES]
+1)
+  inside fragment shader:
+  FragColor = vec4(...) is actually a cycle, check pseudo code:
+
+  for frag_idx in range(len(all_fragments)):
+    FragColor[frag_idx] = vec4(...)\
+
+2)
+why do we need a first line, we don't use it at all...
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compileStatus);
+  glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &infoLength);
+
+3)
+I think this code is about resize handling, but what it really does?
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+
+[Experiments]
+1) Compare performance
+- Compute ModelViewProjection matrices on CPU and the final compileStatus on GPU
+- Give everything to GPU and compute it
+*/
